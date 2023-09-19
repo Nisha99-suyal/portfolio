5c05a7f8c4da8b5ad84e8e81d9e1b973f33c0e4a
Vikas Negi <vikas.n759@techmatrixconsulting.in>
2023-09-19 11:59:45 +0530

    toggle button on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import react, {useState} from "react";
 import { Button } from "react-bootstrap";
 import Logo from "../Images/logo.png";
 import { Container, Navbar } from "react-bootstrap";
@@ -6,19 +6,25 @@ import NavB from "react-bootstrap/Nav";
 import {Link} from "react-router-dom";
 
 export default function Nav() {
+  const [navbarExpanded, setNavbarExpanded] = useState(false);
+
+  const closeNavbar = () => {
+    setNavbarExpanded(false);
+  };
+
   return (
-    <Navbar expand="lg" className="navbar-dark p-0">
+    <Navbar expand="lg" className="navbar-dark p-0" expanded={navbarExpanded}>
       <Container fluid className="px-1">
         <Navbar.Brand href="/">
           <img className="logo" src={Logo} alt="logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"  onClick={() => setNavbarExpanded(!navbarExpanded)}/>
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <NavB className="px-5">
-            <NavB.Link as={Link} to="/">Home</NavB.Link>
-            <NavB.Link as={Link} to="/about">About</NavB.Link>
-            <NavB.Link as={Link} to="/skills">Skills</NavB.Link>
-            <NavB.Link as={Link} to="/contact">Contact</NavB.Link>
+            <NavB.Link as={Link} to="/" onClick={closeNavbar}>Home</NavB.Link>
+            <NavB.Link as={Link} to="/about" onClick={closeNavbar}>About</NavB.Link>
+            <NavB.Link as={Link} to="/skills" onClick={closeNavbar}>Skills</NavB.Link>
+            <NavB.Link as={Link} to="/contact" onClick={closeNavbar}>Contact</NavB.Link>
             <Button
               className="resume-btn"
               onClick={() => {window.open("https://drive.google.com/file/d/1po5W7AWphOHMfo4ymGaNMjdYHuDRoHdi/view?usp=sharing");}}>
