6274315f9746d2002bf79c52f174f39e1c33844b
Nisha Suyal <72795637+Nisha99-suyal@users.noreply.github.com>
2023-12-04 17:38:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
      <div className="protfolio">
       <Nav/>
-      <Routes basename="/portfolio">
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
