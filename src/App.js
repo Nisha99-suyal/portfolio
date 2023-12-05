2ed24f750ad014404903676c267b774113044730
Nisha Suyal <nishasuyal229@gmail.com>
2023-12-05 10:42:59 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import { useEffect } from "react";
 
 function App() {
-  const navigate = useNavigate();
-  useEffect(()=>{
-console.log("HIIIIIertdrgfh");
-    navigate('/');
-  console.log("HIIIII");
-  },[])
   return (
-     <div className="protfolio">
-      <Nav/>
+    <div className="protfolio">
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-     </div> 
+    </div>
   );
 }
 
